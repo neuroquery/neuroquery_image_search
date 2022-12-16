9cf05233b2362f36a7320d96742edbfefce07ad1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 23:19:36 +0000

    Adding tarfile member sanitization to extractall()

--- src/neuroquery_image_search/_datasets.py
+++ src/neuroquery_image_search/_datasets.py
@@ -26,7 +26,26 @@ def _download_data(data_dir):
     archive = data_dir / "neuroquery_image_search_data.tar.gz"
     archive.write_bytes(data)
     with tarfile.open(archive, "r:gz") as f:
-        f.extractall(data_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, data_dir)
     print("Done")
 
 
